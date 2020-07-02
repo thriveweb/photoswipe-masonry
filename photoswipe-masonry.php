98a50aca363f411e3d222bd267d199e2e3540f71
kurtisbradley <36742865+kurtisbradley@users.noreply.github.com>
2020-07-02 14:15:30 +1000

    Small PHP error fix

--- photoswipe-masonry.php
+++ photoswipe-masonry.php
@@ -6,7 +6,7 @@ Description: This is a image gallery plugin for WordPress built using PhotoSwipe
 <a href="http://photoswipe.com/">PhotoSwipe</a>
 Author: Web Design Gold Coast
 Author URI: http://thriveweb.com.au/
-Version: 1.2.10
+Version: 1.2.11
 Text Domain: photoswipe-masonry
 */
 
@@ -464,7 +464,7 @@ function photoswipe_shortcode( $attr ) {
 				$output_buffer .='
 				<figure class="msnry_item" itemscope itemtype="http://schema.org/ImageObject">
 					<a href="'. $full[0] .'" itemprop="contentUrl" data-size="'.$full[1].'x'.$full[2].'" data-caption="'. $image_caption .'" >
-				        <img src='. $thumb[0] .' itemprop="thumbnail" alt="'.$image_alttext.'"  />
+				        <img src="'. $thumb[0] .'" itemprop="thumbnail" alt="'.$image_alttext.'"  />
 				    </a>
 				    <figcaption class="photoswipe-gallery-caption" >'. $image_caption .'</figcaption>
 			    </figure>
