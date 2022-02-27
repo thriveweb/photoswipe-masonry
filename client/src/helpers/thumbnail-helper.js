c7a9ea3c1e1cf9b40bef757ef5a8dac041642c02
Jed Burke <iamthefnx@gmail.com>
2022-02-27 05:19:30 +0200

    Address ESlint issues

--- client/src/helpers/thumbnail-helper.js
+++ client/src/helpers/thumbnail-helper.js
@@ -1,33 +1,28 @@
-'use strict';
-
 // Extracts the width from the input string.
 function parseWidth(thumbWidth, defaultWidth = 150) {
   if (typeof thumbWidth === 'number') {
     return thumbWidth;
   }
-  else if (typeof thumbWidth === 'string') {
+
+  if (typeof thumbWidth === 'string') {
     return parseInt(
       thumbWidth
         .trim()
-        .replace(/width\s?:\s?(\d+)[\w\%]*$/i, '$1')
+        .replace(/width\s?:\s?(\d+)[\w%]*$/i, '$1'),
+      10,
     );
   }
-  else {
-    if (typeof defaultWidth  === 'number')
-    {
-      return defaultWidth
-    }
-    else
-    {
-      throw new Error('Thumbnail width is neither a number or string.');
-    }
+
+  if (typeof defaultWidth === 'number') {
+    return defaultWidth;
   }
+
+  throw new Error('Thumbnail width is neither a number or string.');
 }
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
   module.exports.parseWidth = parseWidth;
-}
-else {
+} else {
   window.ThumbnailHelper = {};
   window.ThumbnailHelper.parseWidth = parseWidth;
 }
